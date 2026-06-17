f90f088d271a58f9ec0b9bcdf20f0c53deff735a
אלעזר גרשי <emg4706@gmail.com>
2026-06-17 17:13:05 +0300

    created class MissionDB & create_mission

---
+++ database/mission_db.py
@@ -0,0 +1,57 @@
+from database.db_connection import DbConnection
+
+
+
+class MissionDB:
+    def __init__(self, connection: DbConnection):
+        self.connection = DbConnection()
+
+    def create_mission(self, data):
+        columns = list([str(key) for key in data])
+        values = list([data[key] for key in data])
+        conn = self.connection.get_connection()
+        cursor = conn.cursor()
+        sql = """
+        INSERT INTO missions (%s) VALUES %s;
+        """
+        cursor.execute(sql, (columns, values))
+        conn.commit()
+        new_id = cursor.lastrowid
+        cursor.execute("""
+        SELECT * FROM missions WHERE id = %s """, new_id)
+        new_mission = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return new_mission
+
+    def get_all_missions(self):
+        conn = self.connection.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        sql = "SELECT * FROM missions;"
+        try:
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            return rows
+        except Exception as e:
+            return f"{e}"
+        finally:
+            cursor.close()
+            conn.close()
+
+
+    def get_mission_by_id(self, id):
+        conn = self.connection.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        sql = """SELECT * FROM missions WHERE id = %s;"""
+        try:
+            cursor.execute(sql, (id,))
+            row = cursor.fetchone()
+            return row
+        except Exception as e:
+            return f"{e}"
+        finally:
+            cursor.close()
+            conn.close()
+
+
+
