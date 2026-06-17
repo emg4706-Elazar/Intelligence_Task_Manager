e7db8e5551a6e459147376504f92e39e4cefcc67
אלעזר גרשי <emg4706@gmail.com>
2026-06-17 16:58:48 +0300

    created get_agent_performance

--- database/agent_db.py
+++ database/agent_db.py
@@ -4,13 +4,13 @@ from database.db_connection import DbConnection
 
 class AgentDB:
     def __init__(self, connection: DbConnection):
-        self.connection = DbConnection().get_connection()
+        self.connection = DbConnection()
 
 
     def create_agent(self, data):
         columns = list([str(key) for key in data])
         values = list([data[key] for key in data])
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor()
         sql = """
         INSERT INTO agents (%s) VALUES %s;
@@ -26,7 +26,7 @@ class AgentDB:
         return new_agent
 
     def get_all_agents(self):
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor(dictionary=True)
         sql = """SELECT * FROM agents;"""
         try:
@@ -41,7 +41,7 @@ class AgentDB:
 
 
     def get_agent_by_id(self, id):
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor(dictionary=True)
         sql = """SELECT * FROM agents WHERE id = %s;"""
         try:
@@ -58,7 +58,7 @@ class AgentDB:
     def update_agent(self, id, data):
         columns = list([str(key) for key in data])
         values = list([data[key] for key in data])
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor()
         sql = """
         UPDATE agents (%s) VALUES %s WHERE id = %s;
@@ -71,7 +71,7 @@ class AgentDB:
         return rowcount > 0
 
     def deactivate_agent(self, id):
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor()
         sql = "UPDATE agents SET is_active = False WHERE id = %s;"
         try:
@@ -86,7 +86,7 @@ class AgentDB:
             conn.close()
 
     def increment_completed(self, id):
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor()
         sql = "UPDATE agents SET completed_missions = %s WHERE id = %s;"
         try:
@@ -103,7 +103,7 @@ class AgentDB:
             conn.close()
 
     def increment_failed(self, id):
-        conn = self.connection
+        conn = self.connection.get_connection()
         cursor = conn.cursor()
         sql = "UPDATE agents SET failed_missions = %s WHERE id = %s;"
         try:
@@ -119,6 +119,16 @@ class AgentDB:
             cursor.close()
             conn.close()
 
+    def get_agent_performance(self, id):
+        agent = self.get_agent_by_id(id)
+        total = agent["completed_missions"] + agent["failed_missions"]
+        dicti = {
+            "total": total,
+            "failed": agent["failed_missions"],
+            "completed": agent["completed_missions"],
+            "success_rate": round((agent["completed_missions"]/total) * 100, 2)
+        }
+        return dicti
     
 
 
@@ -130,5 +140,8 @@ if __name__ == "__main__":
     # print(age_manager.create_agent(data1))
 
     print(age_manager.get_all_agents())
+    print(age_manager.increment_failed(1))
+
     print(age_manager.increment_completed(1))
     print(age_manager.get_agent_by_id(1))
+    print(age_manager.get_agent_performance(1))
